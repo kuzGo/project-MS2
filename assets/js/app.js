03fef0062be545efe9349bb7926d20a32506da1c
kuzGo <gorankuzma467@gmail.com>
2021-05-25 08:40:05 +0000

    Refactor code to render text when button is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,19 @@
-const about = document.querySelector('.about-text1');
+const about = document.querySelector(".about-text1");
 let plusButton = document.querySelector(".about-plus");
 const barmanVid = document.querySelector(".barman-vid");
-const buttonText = document.getElementById('drop');
-const rooftopButton = document.getElementById('drop-season');
+const buttonText = document.getElementById("drop");
+const rooftopButton = document.getElementById("drop-season");
 let closeNavIcon = document.querySelector(".fa-align-right");
 let closeNavButtom = document.querySelector(".navbar-toggler");
+let aboutUs =
+  " Here at London Libre we have a passion for cocktails and travel." +
+  "The founder has traveled the world with cruise ships making" +
+  "cocktails and worked in some London's prestige hotels and" +
+  "cocktail bars. Now, at London Libre bar we can offer a diversity" +
+  "of cocktails and service that fits all the cultures and types of" +
+  "events.We are specialised in weddings, corporate" +
+  "events,private events, festivals and concerts.";
+
 //Movement Animation to happen
 const card = document.querySelector(".card1");
 //Items
@@ -26,45 +35,46 @@ card.addEventListener("mouseleave", (e) => {
 });
 
 //Display about text when "find out" is clicked
-plusButton.addEventListener('click', e => {
-  about.classList.toggle('show-about-text');
+plusButton.addEventListener("click", (e) => {
+  about.classList.toggle("show-about-text");
+  about.innerHTML = aboutUs;
 });
 
-//Playing video animation when hovering mouse 
+//Playing video animation when hovering mouse
 //credit for inspiration https://www.youtube.com/watch?v=dx4oAxaR1As
 
-barmanVid.addEventListener('mouseenter', function (event) {
+barmanVid.addEventListener("mouseenter", function (event) {
   barmanVid.play();
 });
-barmanVid.addEventListener('mouseout', event => {
+barmanVid.addEventListener("mouseout", (event) => {
   barmanVid.pause();
 });
 
-buttonText.addEventListener('mouseover', event => {
-  buttonText.innerText = 'Music Events'
+buttonText.addEventListener("mouseover", (event) => {
+  buttonText.innerText = "Music Events";
 });
-// 
-buttonText.addEventListener('mouseout', event => {
-  buttonText.innerText = "Fancy trying our cocktails?"
-});
-
-rooftopButton.addEventListener('mouseover', event => {
-  rooftopButton.innerText = 'Off Season'
-});
-// Event listener will change text of cocktails button back 
-rooftopButton.addEventListener('mouseout', event => {
-  rooftopButton.innerText = "Always mixing for you"
+//
+buttonText.addEventListener("mouseout", (event) => {
+  buttonText.innerText = "Fancy trying our cocktails?";
 });
 
+rooftopButton.addEventListener("mouseover", (event) => {
+  rooftopButton.innerText = "Off Season";
+});
+// Event listener will change text of cocktails button back
+rooftopButton.addEventListener("mouseout", (event) => {
+  rooftopButton.innerText = "Always mixing for you";
+});
 
-let activeButton = document.querySelectorAll('.nav-link').forEach(activeButton => {
-  activeButton.addEventListener("click", (e) => {
-    activeButton.classList.toggle("active-button");
-
+let activeButton = document
+  .querySelectorAll(".nav-link")
+  .forEach((activeButton) => {
+    activeButton.addEventListener("click", (e) => {
+      activeButton.classList.toggle("active-button");
+    });
   });
-});
 
 // Event listener will change burger button icon when clicked
-closeNavButtom.addEventListener('click', () => {
+closeNavButtom.addEventListener("click", () => {
   closeNavIcon.classList.toggle("fa-times");
-})
+});
