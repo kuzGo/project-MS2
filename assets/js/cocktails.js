cd9d16cb162a4c439c54cf332b8a35efb580e6c6
kuzGo <gorankuzma467@gmail.com>
2021-06-02 22:35:29 +0000

    Error message code refactoring

--- assets/js/cocktails.js
+++ assets/js/cocktails.js
@@ -9,7 +9,7 @@ cocktailName.addEventListener("click", (event) => {
     .then(function (response) {
       // Check if the status is not OK
       if (response.status !== 200) {
-      // Logs the error message of the resonse status
+        // Logs the error message of the resonse status
         console.log("Error fetching data" + response.status);
         return;
       }
@@ -21,9 +21,9 @@ cocktailName.addEventListener("click", (event) => {
     // Catches any errors and logs them
     .catch(function (err) {
       console.log("Fetch Error", err);
+       // Informs the user that something went wrong (If Error found in request)
+      cocktailNameDisplayed.innerHTML = 'Sorry ... Something went wrong :(';
     });
-    // Informs the user that something went wrong (If Error found in request)
-  cocktailNameDisplayed.innerHTML = "Oooh sorry .. Something went wrong :(" ;
 });
 // Function invoked above gets string values from API response
 const getCocktail = function (cocktail) {
@@ -31,7 +31,7 @@ const getCocktail = function (cocktail) {
   cocktailNameDisplayed.innerHTML = cocktail.strDrink;
   // An empty array where ingredients will be pushed
   const ingridients = [];
-// Iterates trough 15 Ingredients and measures
+  // Iterates trough 15 Ingredients and measures
   for (let i = 1; i < 16; i++) {
     // Check if both ingredients and measures have some values 
     if (cocktail[`strIngredient${i}`] && cocktail[`strMeasure${i}`]) {
@@ -43,13 +43,13 @@ const getCocktail = function (cocktail) {
       // Breaks the loop
       break;
     }
-// This code renders response values to the users in card two 
+    // This code renders response values to the users in card two 
     let displayIngrediens = `
         <ul>${ingridients
-          .map(
-            (ingredient) => `<li class="ingredients-list">${ingredient}</li>`
-          )
-          .join("")}</ul>
+        .map(
+          (ingredient) => `<li class="ingredients-list">${ingredient}</li>`
+        )
+        .join("")}</ul>
         <h3 class="ingredients-header">How To:</h3>
         <p class="ingredients-list">${cocktail.strInstructions}</p>
         `;
